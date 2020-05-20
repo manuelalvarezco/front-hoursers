06e47b075b4c1adf47e21795abea73f450dc2256
Manuel Alvarez <alvarezbautista.luis@gmail.com>
2020-05-20 10:50:25 -0500

    por qué elegirnos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,8 +16,9 @@ const routes: Routes = [
         pathMatch: 'full'
       },
       {
-        path: 'home',
-        component: HomeComponent
+          path: 'home',
+          loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+
       },
     ]
   },
